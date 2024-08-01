07a1a076259af1ce33b658af1844894bcdfe0418
placement training <144130483+82828254@users.noreply.github.com>
2024-08-01 21:21:25 +0530

    Create form.html.py

---
+++ 01.08.2024/form.html.py
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Form Page</title>
+</head>
+<body>
+    <form action="/submit" method="post">
+        Name: <input type="text" name="name"><br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
